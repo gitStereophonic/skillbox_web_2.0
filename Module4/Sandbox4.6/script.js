426ca61c34546556999b4df8cbb0629fad913710
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-09-03 22:10:27 +0300

    [Upd] Removed old code

--- Module4/Sandbox4.6/script.js
+++ Module4/Sandbox4.6/script.js
@@ -1,18 +1,3 @@
 $(function () {
-  function changeFontColor() {
-    $('.container').toggleClass('newStyle');
-  }
 
-  $('textarea').on('focus', function () {
-    $(this).css('background-color', 'green');
-  });
-
-  $('textarea').on('blur', function () {
-    $(this).css('background-color', 'white');
-  });
-
-  $('a').on('click', function () {
-    changeFontColor();
-    return false;
-  });
 });
