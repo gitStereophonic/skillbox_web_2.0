8017a10a41bd1492395b33f2298d464e194a68d4
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-09-10 15:58:17 +0300

    [ADD] Get request via JSONP

--- Module4/HW_lesson6/script.js
+++ Module4/HW_lesson6/script.js
@@ -1,84 +1,30 @@
 $(function () {
+  // Из-за того, что бесплатный план fixer.io не позволяет брать другой
+  // base, кроме базового (EUR), приходится заниматься переводами рубля
+  // ручками через евро
+  const myToken = '13a93881c1b3ed4f4b1a5122e58caf99';
+  const cat = 'USD,GBP,RUB';
 
-  const disableScroll = function () {
-    $('body').addClass('scroll-off');
-  }
+  const url = 'http://data.fixer.io/api/latest?access_key=' +
+    myToken + '&symbols=' + cat;
 
-  const enableScroll = function () {
-    $('body').removeClass('scroll-off');
-  }
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'jsonp',
+    success: function (json) {
 
-  $('#changeButton')
-    .css({
-      'margin-left': '20px',
-      'margin-bottom': '40px'
-    }).click(function () {
+      const eur = 1.0;
+      const usd_e = json.rates.USD;
+      const gbp_e = json.rates.GBP;
+      const rub_e = json.rates.RUB;
+
+      const usd_r = rub_e;
       /*
-        I. Hiding objects
-      */
+       * usd = rub
+       */
 
-      // var header = $('.column-left h2');
-
-      /*
-        By css:
-  
-        if (header.css('display') == 'block') {
-          header.css('display', 'none');
-        } else {
-          header.css('display', 'block');
-        }
-      */
-
-      /*
-        By jquery methods:
-  
-        if (header.is(':visible')) {
-          header.hide();
-        } else {
-          header.show(); 
-        }
-      */
-
-      /*
-        Hiding popup container
-      */
-
-      $('#popup-container').fadeIn(400, disableScroll);
-      $('#popup').animate({
-        width: '200px',
-        height: '300px'
-      }, 400);
-
-      /*
-        II. Removing elements
-  
-        $('.column-left h2').remove();
-  
-        var newTitle = $('<h2></h2>');
-        newTitle.text('Срочные новости').css('color', 'green');
-  
-        newTitle.prependTo($('.column-left'));
-      */
-
-      /*
-        Native JS
-      
-        var newTitle = document.createElement('h2');
-        newTitle.innerHTML = 'New Title';
-        newTitle.style.color = 'green';
-  
-        var leftColumn = document.getElementsByClassName('column-left')[0];
-        leftColumn.insertBefore(newTitle, leftColumn.firstChild);
-      */
-    });
-
-  $('#popup-container').click(function (event) {
-    if (event.target == this) {
-      $(this).fadeOut(400, enableScroll);
-      $('#popup').animate({
-        width: 0,
-        height: 0
-      }, 400);
+      $('#fixer_widget').prepend('<h4>EUR: ' + usd_r.toFixed(2) + '</h4>');
     }
   });
 });
