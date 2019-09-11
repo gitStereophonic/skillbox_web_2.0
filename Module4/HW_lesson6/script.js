8dbbb272175c426bd59a914e365c8cf5b3b5de1b
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-09-11 22:23:14 +0300

    [UPD] Ruble's exchange rate added

--- Module4/HW_lesson6/script.js
+++ Module4/HW_lesson6/script.js
@@ -13,18 +13,48 @@ $(function () {
     url: url,
     dataType: 'jsonp',
     success: function (json) {
-
-      const eur = 1.0;
       const usd_e = json.rates.USD;
       const gbp_e = json.rates.GBP;
       const rub_e = json.rates.RUB;
 
-      const usd_r = rub_e;
-      /*
-       * usd = rub
-       */
+      const usd_r = rub_e / usd_e;
+      const gbp_r = rub_e / gbp_e;
 
-      $('#fixer_widget').prepend('<h4>EUR: ' + usd_r.toFixed(2) + '</h4>');
+      var cat = $('<h4></h4>');
+      cat.addClass('news-item-text');
+      cat.text(
+        'EUR: ' + rub_e.toFixed(2) + ', USD: ' + usd_r.toFixed(2)
+        + ', GBP: ' + gbp_r.toFixed(2)
+      );
+
+      var tag = $('<a></a>');
+      tag.prop("href", "#");
+      tag.text('/ Экономика');
+
+      var date = new Date();
+      var time = $('<div></div>');
+      time.addClass('news-time');
+      time.text(date.getHours() + ':' + date.getMinutes());
+
+      var props = $('<div></div>');
+      props.addClass('news-item-props');
+      props.append(tag);
+      props.append(time);
+
+      var body = $('<article></article>');
+      body.addClass('news-item');
+      body.append(cat);
+      body.append(props);
+
+      var header = $('<h2></h2>');
+      header.text('Курс рубля');
+
+      var finance = $('<div></div>');
+      finance.append($('<hr>'));
+      finance.append(header);
+      finance.append(body);
+
+      $('.column-left').append(finance);
     }
   });
 });
