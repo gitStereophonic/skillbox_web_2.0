49a6947f1084a47526bf4c1978e7621286329aad
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-10-15 21:16:19 +0300

    [Fix] HW5. Scroll block

--- Module4/HW_lesson5/script.js
+++ Module4/HW_lesson5/script.js
@@ -1,84 +1,44 @@
-$(function () {
-
-  const disableScroll = function () {
-    $('body').addClass('scroll-off');
-  }
-
-  const enableScroll = function () {
-    $('body').removeClass('scroll-off');
-  }
-
+$(function() {
   $('#changeButton')
     .css({
       'margin-left': '20px',
       'margin-bottom': '40px'
-    }).click(function () {
-      /*
-        I. Hiding objects
-      */
+    })
+    .click(function() {
+      let pagePos = $(window).scrollTop();
 
-      // var header = $('.column-left h2');
+      $('body')
+        .addClass('scroll-off')
+        .attr('data-scroll', pagePos);
+      $('body').css('top', pagePos);
 
-      /*
-        By css:
-  
-        if (header.css('display') == 'block') {
-          header.css('display', 'none');
-        } else {
-          header.css('display', 'block');
-        }
-      */
-
-      /*
-        By jquery methods:
-  
-        if (header.is(':visible')) {
-          header.hide();
-        } else {
-          header.show(); 
-        }
-      */
-
-      /*
-        Hiding popup container
-      */
-
-      $('#popup-container').fadeIn(400, disableScroll);
-      $('#popup').animate({
-        width: '200px',
-        height: '300px'
-      }, 400);
-
-      /*
-        II. Removing elements
-  
-        $('.column-left h2').remove();
-  
-        var newTitle = $('<h2></h2>');
-        newTitle.text('Срочные новости').css('color', 'green');
-  
-        newTitle.prependTo($('.column-left'));
-      */
-
-      /*
-        Native JS
-      
-        var newTitle = document.createElement('h2');
-        newTitle.innerHTML = 'New Title';
-        newTitle.style.color = 'green';
-  
-        var leftColumn = document.getElementsByClassName('column-left')[0];
-        leftColumn.insertBefore(newTitle, leftColumn.firstChild);
-      */
+      $('#popup-container').fadeIn(400);
+      $('#popup').animate(
+        {
+          width: '200px',
+          height: '300px'
+        },
+        400
+      );
     });
 
-  $('#popup-container').click(function (event) {
+  $('#popup-container').click(function(event) {
     if (event.target == this) {
-      $(this).fadeOut(400, enableScroll);
-      $('#popup').animate({
-        width: 0,
-        height: 0
-      }, 400);
+      let pos = parseInt($('body').attr('data-scroll'), 10);
+      $(this).fadeOut(400);
+      $('#popup').animate(
+        {
+          width: 0,
+          height: 0
+        },
+        400,
+        function() {
+          $('body').removeClass('scroll-off');
+          $('body')
+            .css('top', 'auto')
+            .removeAttr('data-scroll');
+        }
+      );
     }
   });
 });
