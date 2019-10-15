cb7d22f607a1e555ae059c864b74e6e2bc4cae24
Sergey Chinkov <sergey.chinkov@yandex.ru>
2019-10-15 20:21:25 +0300

    [Fix] Fixed HW4. Minimized code + alert

--- Module4/HW_lesson4/script.js
+++ Module4/HW_lesson4/script.js
@@ -1,25 +1,22 @@
-$(function () {
+$(function() {
   // Функция, вставляющая tool tip с информацией о пустом списке дел
   function addEmptyList() {
-    var emptyList = $('<p></p>');
-    emptyList.text('Список пуст...');
-    emptyList.addClass('tips empty-list');
-    $('#task-list').append(emptyList);
+    $('#task-list').append('<p class="tips empty-list">Список пуст...</p>');
   }
 
   // При загрузке страницы список дел пустой
   addEmptyList();
 
   // Событие добавления задачи в список дел
-  $('#add-task').click(function () {
-
+  $('#add-task').click(function() {
     // Заголовок дела из инпута
-    var newTaskTitle = $('#new-task-name').val();
+    const newTaskTitle = $('#new-task-name').val();
     // Описание дела из инпута
-    var newTaskDescription = $('#new-task-desc').val();
+    const newTaskDescription = $('#new-task-desc').val();
 
     // Проверка непустого заголовка
-    if (newTaskTitle == '') {
+    if (newTaskTitle.length === 0) {
+      alert('Нельзя добавить пустое дело!');
       return false;
     }
 
@@ -28,16 +25,14 @@ $(function () {
       $('.empty-list').remove();
     }
 
-    // HTML заголовок с текстом главной цели дела 
-    var newTaskH = $('<h3></h3>');
-    newTaskH.text(newTaskTitle);
-
     // "Кнопка" удаления всего контейнера
-    var closeBtn = $('<button></button>');
-    closeBtn.addClass('close-btn');
-    closeBtn.click(function () {
-      $(this).parent().parent().remove();
-      // Если удаленный элемент был последний, добавляется 
+    const closeBtn = $('<button class="close-btn"></button>');
+    closeBtn.click(function() {
+      $(this)
+        .parent()
+        .parent()
+        .remove();
+      // Если удаленный элемент был последний, добавляется
       // tool tip с пометкой, что список пуст
       if (!$('.task-container').length) {
         addEmptyList();
@@ -45,37 +40,35 @@ $(function () {
     });
 
     // "Кнопка", скрывающая описание задания
-    var optionsBtn = $('<button></button>');
-    optionsBtn.addClass('options-btn');
-    optionsBtn.click(function () {
+    const optionsBtn = $('<button class="options-btn"></button>');
+    optionsBtn.click(function() {
       // Триггер появления/исчезновения описания
-      $(this).parent().parent().children('.task-description').slideToggle('slow');
+      $(this)
+        .parent()
+        .parent()
+        .children('.task-description')
+        .slideToggle('slow');
 
       // Поворот стрелки
-      if ($(this).hasClass('rotated'))
-        $(this).removeClass('rotated');
-      else
-        $(this).addClass('rotated');
+      $(this).toggleClass('rotated');
     });
 
-    // Хедер контейнера задачи. Содержит текст задачи и кнопки 
+    // Хедер контейнера задачи. Содержит текст задачи и кнопки
     // удаления задачи и сворачивания описания
-    var newTaskHeader = $('<div></div>');
-    newTaskHeader.addClass('new-task-header clearfix');
-    newTaskHeader.append(newTaskH);   // Текст основной цели
-    newTaskHeader.append(closeBtn);   // Удаление
+    const newTaskHeader = $(`
+      <div class="new-task-header clearfix">
+        <h3>${newTaskTitle}</h3>
+      </div>
+    `);
+    newTaskHeader.append(closeBtn); // Удаление
     newTaskHeader.append(optionsBtn); // Сворачивание
 
-    // Контейнер с описанием задачи
-    var newTaskDescD = $('<div></div>');
-    newTaskDescD.addClass('task-description');
-    newTaskDescD.text(newTaskDescription);  // Текст описания
-
     // Контейнер задачи. Содержит хедер и описание
-    var newTaskDiv = $('<div></div>');
-    newTaskDiv.addClass('task-container');
+    const newTaskDiv = $('<div class="task-container"></div>');
     newTaskDiv.append(newTaskHeader); // Хедер задачи с элементами управления
-    newTaskDiv.append(newTaskDescD);  // Описание задачи
+    newTaskDiv.append(`
+      <div class="task-description">${newTaskDescription}</div>
+    `); // Описание задачи
 
     // Добавление задачи на страницу
     newTaskDiv.appendTo($('#task-list'));
